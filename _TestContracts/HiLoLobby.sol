1d0581a1839132118f13a0b66621e5fbf20e4199
Julio Ramirez <julio1031.jr@gmail.com>
2022-01-02 21:23:38 -0800

    Update HiLoLobby.sol

--- _TestContracts/HiLoLobby.sol
+++ _TestContracts/HiLoLobby.sol
@@ -20,16 +20,17 @@ pragma solidity ^0.8.0;
 
 
 contract HiLoLobby {
-    enum LobbyState { Open, Closed, Ended}
+    enum LobbyState { Open, Closed, Collections,Ended}
     LobbyState public currentState;
     address admin;
     address payable[] players;
     address winner;
     address loser;
-    uint rollResultCount;
+    uint8 rollResultCount;
     uint rollSize;
     uint fee = 0.005 ether;
-    uint payout;
+    uint winnerPayout;
+    uint8 minPlayers = 2; 
 
     mapping(address => bool) isRegistered;
     mapping(address => uint256) playerRoll;
@@ -51,42 +52,91 @@ contract HiLoLobby {
         require(!isRegistered[msg.sender], "Address is already registered. Only 1 entry per address allowed");
         require(address(msg.sender).balance >= fee, "Not enough funds" );
         isRegistered[msg.sender] = true;
+        players.push(payable(msg.sender));
     }
-    function startGame() internal{
+    function startGame() external{
         require(msg.sender == admin, "Only the host may start the game");
         currentState = LobbyState.Closed;
         //generate rolls 
     }
     function roll() external{
-        require(currentState == LobbyState.Closed, "The Lobby is not Rolling Yet");
         require(isRegistered[msg.sender], "This address is not a part of this lobby");
+        require(currentState == LobbyState.Closed, "The Lobby is not open for rolling");
         require(!blacklist[msg.sender],"Already Rolled");
         playerRoll[address(msg.sender)] = uint(random() % rollSize +1);
         blacklist[msg.sender] = true ;
         rollResultCount++;
-        endGame();
+        if(rollResultCount == players.length){
+            _endGame();
+        }  
     }
 
-    function endGame() internal{
-        require(rollResultCount == players.length);
-        winner = getWinner();
-        loser = getLoser();
-        payout = uint(playerRoll[winner]- playerRoll[loser]);
+    function _endGame() internal {
+        getWinner();
+        getLoser();
+        winnerPayout = uint(playerRoll[winner]- playerRoll[loser]);
+        assignPayouts();
+        currentState = LobbyState.Collections;
+    }
+
+    function endGame() external {
+        require(msg.sender == admin, "Only admim may end the game after all players roll");
+        require(currentState == LobbyState.Closed,"The lobby Must be closed to end the game");
+        require(rollResultCount == players.length, "Wait for all players for finish rolling");
+        _endGame();
     }
     function collect() external {
-        
+        require(isRegistered[msg.sender], "This address is not a part of this lobby");
+        require(currentState == LobbyState.Collections, "The Lobby is not open for collections yet");
+        require(msg.sender != loser, "This address lost and will receive no payout");
+
+    }
+    function getWinner() internal{
+        bool winnerTie;
+        bool loserTie;
+        for(uint8 i = 0; i < players.length; i++){
+            if(i == 0){
+                winner = players[i];
+                loser = players[i];
+                continue;
+            }
+            if(playerRoll[players[i]] >= playerRoll[winner]){
+                if((playerRoll[players[i]] == playerRoll[winner])){
+                    winnerTie == true;
+                }else{
+                    winnerTie == false;
+                }
+            }
+            if(playerRoll[players[i]] <= playerRoll[loser]){
+                if((playerRoll[players[i]] == playerRoll[loser])){
+                    loserTie == true;
+                }
+                loserTie == false;    
+            }
+        }
+    }
+    function getLoser() internal{ 
+    }
+    function assignPayouts() internal{
+        //maybe first assign winner payout,
+        for(uint8 i = 0; i < players.length; i++){
+            require(isRegistered[players[i]], "Could not assign payout, due to unregistered address");
+            if(players[i]!=winner && players[i]!=loser){
+                transfer(players[i], address(this).balance/(players.length - 2));
+            }
+        }
     }
     function transfer(address payable _to, uint _amount) internal{
         (bool success, ) = _to.call{value: _amount}("");
         require(success, "Failed to send Ether");
     }
-    function random() internal view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp,players.length)));   
+    function random() internal view returns(uint256){
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp,players.length)));   
     }
     function playerCount()view external returns(uint8){
         return uint8(players.length);
     }
-     receive() external payable{
+    receive() external payable{
         buyIn();    
     }
 }
